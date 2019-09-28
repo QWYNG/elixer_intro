830f3c9ac0296d5bd5857b2f4fadbdc438483dda
qwyng <ikusawasi@gmail.com>
2019-09-28 22:03:02 +0900

    add module element

--- chapter13/issues/lib/issues/github_issues.ex
+++ chapter13/issues/lib/issues/github_issues.ex
@@ -1,4 +1,6 @@
 defmodule Issues.GithubIssues do
+  @github_url "https://api.github.com"
+
   def fetch(user, project) do
     issues_url(user, project)
     |> HTTPoison.get
@@ -6,7 +8,7 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({ :ok, %{status_code: 200 , body: body }}) do
