75c7f67061a853a33728d2514fc3f2cdccb86c78
qwyng <ikusawasi@gmail.com>
2019-10-02 22:30:53 +0900

    format table

--- chapter13/issues/lib/issues/cli.ex
+++ chapter13/issues/lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
@@ -40,7 +42,8 @@ defmodule Issues.CLI do
     |> decode_response
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
-    |> Enum.take(count)
+    |> Enum.take
+    |> print_table_for_columns(~w(number created_at title))
   end
 
   def decode_response({ :ok, body}), do: body
